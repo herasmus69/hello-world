9f7af529dfe562b46d9ec6f9bf35dcab9685d6ff
Rasmussen <henrik.t.rasmussen@siemens.com>
2016-12-05 17:25:55 +0100

    ArrayListToStringDemo.java

--- ArrayListToStringDemo.java
+++ ArrayListToStringDemo.java
@@ -1,4 +1,10 @@
-class myclass
-public void main(String arg...) {
-	system.out.println("Test");
+import java.util.*;
+
+class ArrayListToStringDemo {
+	public static void main(String... args) {
+		System.out.println("Testing how a ArrayList toString look");
+		List<Integer> list = new ArrayList<>();
+		list.add(5);
+		System.out.println("List containing 5: " + list);		
+	}
 }
